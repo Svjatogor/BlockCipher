689f41ae513e525b2a77681f844e70f60652b3a3
Timofey <vsjo.zanjato@gmail.com>
2016-09-26 00:31:01 +0300

    Add funclions to convert bit and byte array

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <QCoreApplication>
 #include <QTextStream>
+#include <QBitArray>
+#include <QVariant>
+#include <QByteArray>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QTextStream out(stdout);
-    out << "Hello" << endl;
+    char *text = "AB";
+    QBitArray b_test = QBitArray(5);
+    b_test[0] = 1;
+    b_test[1] = 1;
+    QVariant v(b_test);
+    QBitArray b_array = v.toBitArray();
+    b_array ^= QBitArray(b_array.size(), true);
+    b_array ^= QBitArray(b_array.size(), true);
+    QString convert_text = QVariant(b_array).toString();
     return a.exec();
 }
